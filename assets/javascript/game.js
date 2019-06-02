9a896f2efe86c2579a107f79380950e80ace8573
Steve Archibald <stevearchi21@gmail.com>
2019-06-02 13:26:36 -0600

    words are now capitalized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,9 @@
 //document.getElementById("begin").textContent = "Press enter to play!";
 
 // creates array holding all possible words/phrases to be guessed
-var allWords = ["path to exile", "serum visions", "scapeshift", "arclight phoenix", "cavern of souls", "tarmogoyf", "dark confidant",
-    "scavenging ooze", "thoughtseize", "inquisition of kozilek", "liliana of the veil", "jace, the mind sculptor", "remand",
-    "mana leak", "opt", "snapcaster mage", "wurmcoil engine"
-];
+var allWords = ["Path to Exile", "Serum Visions", "Scapeshift", "Arclight Phoenix", "Cavern of Souls", "Tarmogoyf", "Dark Confidant",
+    "Scavenging Ooze", "Thoughtseize", "Inquisition of Kozilek", "Liliana of the Veil", "Jace, the Mind Scultor", "Remand",
+    "Mana Leak", "Opt", "Snapcaster Mage", "Wurmcoil Engine"];
 var splashPageOn = false; /// Set to true before submitting
 var beingGuessed = []; // array which will hold all letters of word being guessed
 var finishedRound = true; // tells the game if a new word should be picked
@@ -36,16 +35,18 @@ document.onkeyup = function (event) {
         document.getElementById("begin").textContent = "";
         splashPageOn = false;
     }
+    
     if (splashPageOn === false) {
         // display instructions  
-        document.getElementById("instructions").textContent = "Press a letter or the spacebar";
+       
+   
         if (finishedRound) {
             // reset variables
             partialWord = "";
             guessesRemaining = 0;
             lettersGuessed = "";
             wordBeingGuessed = allWords[Math.floor(Math.random() * allWords.length)]; // generate random number to determine which word to be used
-
+            guessesRemaining = wordBeingGuessed.length*3;
             for (i = 0; i < wordBeingGuessed.length; i++) {
                 partialWord = setCharAt(partialWord, i, '-'); // display underscores for each letter not yet guessed
             }
@@ -57,30 +58,48 @@ document.onkeyup = function (event) {
             // when user guesses a correct letter, display the letter in place of dash
 
             for (i = 0; i < wordBeingGuessed.length; i++) {
-                if (event.key === wordBeingGuessed[i]) {
+                if (event.key === wordBeingGuessed[i].toLowerCase()) {
                     partialWord = setCharAt(partialWord, i, wordBeingGuessed[i]);
                 }
             }
             Displaypartialword();
-
-        }
-
-
-        for (i = 0; i < letters.length; i++) {
-            if (event.key === letters[i].toLocaleLowerCase()) {
-                lettersGuessed = lettersGuessed.concat(", ", event.key);
-                
+            
+            for (i = 0; i < letters.length; i++) {
+                if (event.key === letters[i].toLocaleLowerCase()) {
+                    lettersGuessed = lettersGuessed.concat(", ", event.key);
+                }
+    
             }
-          
         }
 
-        document.getElementById("guessesRemaining").textContent = guessesRemaining;
-        document.getElementById("lettersGuessed").textContent = "Letters Guessed: ";
-        document.getElementById("wins").textContent = wins;
-        document.getElementById("actualLettersGuessed").textContent = lettersGuessed;
+        
 
+        document.getElementById("guessesRemaining").textContent = "Guesses remaining: " + guessesRemaining;
+        document.getElementById("lettersGuessed").textContent = "Letters Guessed: ";
+        document.getElementById("wins").textContent = "Wins: " + wins;
+        document.getElementById("actualLettersGuessed").textContent = lettersGuessed;
+        document.getElementById("instructions").textContent = "Press a letter or the spacebar"; 
+    }
+    
+    if (partialWord === wordBeingGuessed){
+        finishedRound = true;
+        wins++;
+        document.getElementById("win").textContent = "You guessed " + wordBeingGuessed + ".  Great job!";
+        document.getElementById("playAgain").textContent = "Press any key to play again.";
+                                                    
+        //alert("You guessed " + wordBeingGuessed + " Great job!")
+    } else{
+        document.getElementById("win").textContent = "";
+        document.getElementById("playAgain").textContent = "";
     }
 
+    
+    if (guessesRemaining <= 0){
+        document.getElementById("win").textContent = "You lost :( The card was: " + wordBeingGuessed;
+        document.getElementById("playAgain").textContent = "Press any key to play again.";
+        finishedRound = true;
+    }
+    guessesRemaining--;
 }
 
 
