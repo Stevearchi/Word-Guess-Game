1559257b812da1c4ca9d57fe48d650fded8f897f
Steve Archibald <stevearchi21@gmail.com>
2019-06-02 15:04:02 -0600

    added some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,21 +7,31 @@
 //document.getElementById("begin").textContent = "Press enter to play!";
 
 // creates array holding all possible words/phrases to be guessed
-var allWords = ["Path to Exile", "Serum Visions", "Scapeshift", "Arclight Phoenix", "Cavern of Souls", "Tarmogoyf", "Dark Confidant",
-    "Scavenging Ooze", "Thoughtseize", "Inquisition of Kozilek", "Liliana of the Veil", "Jace, the Mind Scultor", "Remand",
-    "Mana Leak", "Opt", "Snapcaster Mage", "Wurmcoil Engine"];
+var allWords = ["Path to Exile",
+// "Serum Visions", "Scapeshift", 
+// "Arclight Phoenix", "Cavern of Souls", "Tarmogoyf", "Dark Confidant",
+//     "Scavenging Ooze", "Thoughtseize", "Inquisition of Kozilek", "Liliana of the Veil", "Jace, the Mind Scultor", "Remand",
+//     "Mana Leak", "Opt", "Snapcaster Mage", "Wurmcoil Engine"
+];
 var splashPageOn = false; /// Set to true before submitting
 var beingGuessed = []; // array which will hold all letters of word being guessed
 var finishedRound = true; // tells the game if a new word should be picked
 var partialWord = "";
+var partialWordSpaced = "";
 var wordBeingGuessed = "";
 var guessesRemaining = 0;
 var lettersGuessed = "";
 var wins = 0;
-var letters = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var imageName = "";
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 function Displaypartialword() {
-    document.getElementById("beingGuessed").textContent = partialWord;
+    // for (i=0; i<partialWord.length; i++){
+    //    partialWordSpaced[i] = &nbsp;
+
+    //    // partialWordSpaced[2*i] = partialWord[i];
+    // }
+    document.getElementById("beingGuessed").textContent = partialWord.split('').join(' ');
 }
 // function which changes a character at a specified index in a string.  
 function setCharAt(str, index, chr) {
@@ -35,24 +45,78 @@ document.onkeyup = function (event) {
         document.getElementById("begin").textContent = "";
         splashPageOn = false;
     }
-    
     if (splashPageOn === false) {
         // display instructions  
-       
-   
+
         if (finishedRound) {
             // reset variables
             partialWord = "";
             guessesRemaining = 0;
             lettersGuessed = "";
-            wordBeingGuessed = allWords[Math.floor(Math.random() * allWords.length)]; // generate random number to determine which word to be used
-            guessesRemaining = wordBeingGuessed.length*3;
+            var randomWordNumber = Math.floor(Math.random() * allWords.length);
+            wordBeingGuessed = allWords[randomWordNumber]; // generate random number to determine which word to be used
+            //This switch statement sets the path for the image corresponding to the random word
+            // I think this could be simpler code if I made an object members in this format: cardname: imagename
+            switch (randomWordNumber) {
+                case 0:
+                    imageName = "path-to-exile";
+                    break;
+                case 1:
+                    imageName = "serum-visions";
+                    break;
+                case 2:
+                    imageName = "scapeshift";
+                    break;
+                case 3:
+                    imageName = "arclight-phoenix"
+                    break;
+                case 4:
+                    imageName = "cavern-of-souls"
+                    break;
+                case 5:
+                    imageName = "tarmogoyf";
+                    break;
+                case 6:
+                    imageName = "dark-confidant";
+                    break;
+                case 7:
+                    imageName = "scavenging-ooze";
+                    break;
+                case 8:
+                    imageName = "thoughtseize"
+                    break;
+                case 9:
+                    imageName = "inquisition-of-kozilek";
+                    break;
+                case 10:
+                    imageName = "liliana-of-the-veil";
+                    break;
+                case 11:
+                    imageName = "remand";
+                    break;
+                case 12:
+                    imageName = "mana-leak";
+                    break;
+                case 13:
+                    imageName = "opt";
+                    break;
+                case 14:
+                    imageName = "snapcaster-mage";
+                    break;
+                case 15:
+                    imageName = "wurmcoil-engine"
+            }
+
+
+
+
+            guessesRemaining = wordBeingGuessed.length * 3;
             for (i = 0; i < wordBeingGuessed.length; i++) {
-                partialWord = setCharAt(partialWord, i, '-'); // display underscores for each letter not yet guessed
+                partialWord = setCharAt(partialWord, i, '_'); // display underscores for each letter not yet guessed
             }
             finishedRound = false;
             Displaypartialword();
-            console.log(wordBeingGuessed)
+
         } else {
 
             // when user guesses a correct letter, display the letter in place of dash
@@ -63,43 +127,48 @@ document.onkeyup = function (event) {
                 }
             }
             Displaypartialword();
-            
+
             for (i = 0; i < letters.length; i++) {
                 if (event.key === letters[i].toLocaleLowerCase()) {
-                    lettersGuessed = lettersGuessed.concat(", ", event.key);
+                    lettersGuessed = lettersGuessed.concat(" ", event.key);
                 }
-    
+
             }
         }
 
-        
+
 
         document.getElementById("guessesRemaining").textContent = "Guesses remaining: " + guessesRemaining;
         document.getElementById("lettersGuessed").textContent = "Letters Guessed: ";
         document.getElementById("wins").textContent = "Wins: " + wins;
         document.getElementById("actualLettersGuessed").textContent = lettersGuessed;
-        document.getElementById("instructions").textContent = "Press a letter or the spacebar"; 
+        document.getElementById("instructions").textContent = "Press a letter or the spacebar";
     }
-    
-    if (partialWord === wordBeingGuessed){
+
+    // If user wins
+    if (partialWord === wordBeingGuessed) {
         finishedRound = true;
         wins++;
+        document.getElementById("image").innerHTML = '<img class = "cardImage" src="assets/images/' + imageName + '.jpg">';
         document.getElementById("win").textContent = "You guessed " + wordBeingGuessed + ".  Great job!";
         document.getElementById("playAgain").textContent = "Press any key to play again.";
-                                                    
-        //alert("You guessed " + wordBeingGuessed + " Great job!")
-    } else{
+
+    } else { //clears the win/lose text and image
         document.getElementById("win").textContent = "";
         document.getElementById("playAgain").textContent = "";
     }
 
-    
-    if (guessesRemaining <= 0){
+    if (guessesRemaining <= 0) {
         document.getElementById("win").textContent = "You lost :( The card was: " + wordBeingGuessed;
         document.getElementById("playAgain").textContent = "Press any key to play again.";
         finishedRound = true;
     }
-    guessesRemaining--;
+    for (i = 0; i < letters.length; i++) {
+        if (event.key === letters[i].toLowerCase()) {
+            guessesRemaining--;
+        }
+    }
+    
 }
 
 
