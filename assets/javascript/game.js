5a5fb37bf0f410824366e792bc68909f91cbded9
Steve Archibald <stevearchi21@gmail.com>
2019-06-02 16:06:11 -0600

    fixed spacing issue with partialWord

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,49 +4,44 @@
 
 
 
-//document.getElementById("begin").textContent = "Press enter to play!";
+document.getElementById("begin").textContent = "Press enter to play";
 
 // creates array holding all possible words/phrases to be guessed
 var allWords = ["Path to Exile",
-// "Serum Visions", "Scapeshift", 
-// "Arclight Phoenix", "Cavern of Souls", "Tarmogoyf", "Dark Confidant",
-//     "Scavenging Ooze", "Thoughtseize", "Inquisition of Kozilek", "Liliana of the Veil", "Jace, the Mind Scultor", "Remand",
-//     "Mana Leak", "Opt", "Snapcaster Mage", "Wurmcoil Engine"
+    "Serum Visions", "Scapeshift",
+    "Arclight Phoenix", "Cavern of Souls", "Tarmogoyf", "Dark Confidant",
+    "Scavenging Ooze", "Thoughtseize", "Inquisition of Kozilek", "Liliana of the Veil", "Remand",
+    "Mana Leak", "Opt", "Snapcaster Mage", "Wurmcoil Engine"
 ];
-var splashPageOn = false; /// Set to true before submitting
+var splashPageOn = true;
 var beingGuessed = []; // array which will hold all letters of word being guessed
 var finishedRound = true; // tells the game if a new word should be picked
 var partialWord = "";
 var partialWordSpaced = "";
 var wordBeingGuessed = "";
-var guessesRemaining = 0;
+var guessesRemaining = 1;
 var lettersGuessed = "";
 var wins = 0;
 var imageName = "";
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 function Displaypartialword() {
-    // for (i=0; i<partialWord.length; i++){
-    //    partialWordSpaced[i] = &nbsp;
-
-    //    // partialWordSpaced[2*i] = partialWord[i];
-    // }
-    document.getElementById("beingGuessed").textContent = partialWord.split('').join(' ');
+    document.getElementById("beingGuessed").textContent = partialWord.split('').join(' '); // split/join adds a space inbetween each letter
 }
 // function which changes a character at a specified index in a string.  
+
 function setCharAt(str, index, chr) {
     //if(index > str.length-1) return str;
     return str.substr(0, index) + chr + str.substr(index + 1); // returns the new string.
 }
 document.onkeyup = function (event) {
-    // clears 'Press any en)ter get started'
+    // clears 'Press any enter get started'
 
     if (splashPageOn && event.keyCode === 13) { // keycode 13 = enter
         document.getElementById("begin").textContent = "";
         splashPageOn = false;
     }
     if (splashPageOn === false) {
-        // display instructions  
 
         if (finishedRound) {
             // reset variables
@@ -107,13 +102,18 @@ document.onkeyup = function (event) {
                     imageName = "wurmcoil-engine"
             }
 
-
-
-
             guessesRemaining = wordBeingGuessed.length * 3;
+            console.log(wordBeingGuessed)
             for (i = 0; i < wordBeingGuessed.length; i++) {
-                partialWord = setCharAt(partialWord, i, '_'); // display underscores for each letter not yet guessed
+                if (wordBeingGuessed[i] === " ") {
+                    partialWord = setCharAt(partialWord, i, '\xa0') // /xa0 is a non-breakable space. this adds a space to partial word        
+                } else {
+                    partialWord = setCharAt(partialWord, i, '_'); // display underscores for each letter not yet guessed
+                }
+
             }
+
+            // partialWord = setCharAt(partialWord, i, " ");
             finishedRound = false;
             Displaypartialword();
 
@@ -122,7 +122,7 @@ document.onkeyup = function (event) {
             // when user guesses a correct letter, display the letter in place of dash
 
             for (i = 0; i < wordBeingGuessed.length; i++) {
-                if (event.key === wordBeingGuessed[i].toLowerCase()) {
+                if (event.key === wordBeingGuessed[i].toLowerCase() && event.key !== " ") {
                     partialWord = setCharAt(partialWord, i, wordBeingGuessed[i]);
                 }
             }
@@ -135,42 +135,43 @@ document.onkeyup = function (event) {
 
             }
         }
-
-
-
+        // displays text on the left
         document.getElementById("guessesRemaining").textContent = "Guesses remaining: " + guessesRemaining;
         document.getElementById("lettersGuessed").textContent = "Letters Guessed: ";
         document.getElementById("wins").textContent = "Wins: " + wins;
         document.getElementById("actualLettersGuessed").textContent = lettersGuessed;
-        document.getElementById("instructions").textContent = "Press a letter or the spacebar";
-    }
+        document.getElementById("instructions").textContent = "Press a letter to guess if it's in the card name";
 
-    // If user wins
-    if (partialWord === wordBeingGuessed) {
-        finishedRound = true;
-        wins++;
-        document.getElementById("image").innerHTML = '<img class = "cardImage" src="assets/images/' + imageName + '.jpg">';
-        document.getElementById("win").textContent = "You guessed " + wordBeingGuessed + ".  Great job!";
-        document.getElementById("playAgain").textContent = "Press any key to play again.";
+        // If user wins
+        if (partialWord.replace(/\s/g, '') === wordBeingGuessed.replace(/\s/g, '')) {
+            finishedRound = true;
+            wins++;
+            document.getElementById("image").innerHTML = '<img class = "cardImage" src="assets/images/' + imageName + '.jpg">';
 
-    } else { //clears the win/lose text and image
-        document.getElementById("win").textContent = "";
-        document.getElementById("playAgain").textContent = "";
-    }
+            document.getElementById("win").textContent = "You guessed " + wordBeingGuessed + ".  Great job!";
+            document.getElementById("playAgain").textContent = "Press any key to play again.";
 
-    if (guessesRemaining <= 0) {
-        document.getElementById("win").textContent = "You lost :( The card was: " + wordBeingGuessed;
-        document.getElementById("playAgain").textContent = "Press any key to play again.";
-        finishedRound = true;
-    }
-    for (i = 0; i < letters.length; i++) {
-        if (event.key === letters[i].toLowerCase()) {
-            guessesRemaining--;
+        } else { //clears the win/lose text and image
+            document.getElementById("win").textContent = "";
+            document.getElementById("playAgain").textContent = "";
         }
-    }
-    
-}
 
+        if (guessesRemaining <= 0) {
+            document.getElementById("win").textContent = "You lost :( The card was: " + wordBeingGuessed;
+            document.getElementById("playAgain").textContent = "Press any key to play again.";
+            document.getElementById("image").innerHTML = '<img class = "cardImage" src="assets/images/' + imageName + '.jpg">';
+            finishedRound = true;
+        }
+        for (i = 0; i < letters.length; i++) {
+            if (event.key === letters[i].toLowerCase()) {
+                guessesRemaining--;
+            }
+        }
+
+    }
+
+
+}
 
 
 
